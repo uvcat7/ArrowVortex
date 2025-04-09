429bcb794d2e06df7744684346f9130ea35aebfb
Psycast <jonathanjwhite@hotmail.com>
2025-04-09 05:26:14 -0230

    Parity-2017: End Row calculation + sub-beat lines.
    - End Row calculation wasn't factoring in notes.
    - Sub-beats lines shouldn't be drawn past End Row.

--- src/Managers/SimfileMan.cpp
+++ src/Managers/SimfileMan.cpp
@@ -86,6 +86,15 @@ void myUpdateEndRow()
 		}
 	}
 
+	// Row of the last note.
+	if(myChart) 
+	{
+		for (auto& n : myChart->notes)
+		{
+			endRow = max(endRow, n.endrow);
+		}
+	}
+
 	// Row of the end of the music.
 	endRow = max(endRow, gTempo->timeToRow(gMusic->getSongLength()));
 
