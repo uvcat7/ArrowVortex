aa609514d226724ad05ac966261e39c6c88a6e42
uvcat <theultravioletcatastrophe10@gmail.com>
2025-07-08 12:24:41 -0700

    updated per sukibaby's review.

--- src/Simfile/SaveSm.cpp
+++ src/Simfile/SaveSm.cpp
@@ -514,8 +514,9 @@ static void GetSectionCompression(const char* section, int width, std::list<uint
 			float mod = (float) ROWS_PER_NOTE_SECTION / i;
 			for (int j = 0; valid && j < ROWS_PER_NOTE_SECTION; ++j)
 			{
+				float rem = round(fmod(j, mod));
 				// Check all the compressed rows and make sure they are empty
-				if ((int)(round(fmod(j, mod))) > 0 && (int)(round(fmod(j, mod))) < (int) mod
+				if (rem > 0 && rem < static_cast<int>(mod)
 					&& memcmp(section + j * width, zeroline.str(), width))
 				{ 
 					valid = false;
