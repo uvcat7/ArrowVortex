9b6fa98768fb81aa157be256b5912664462b0ddf
a <a@a>
2025-07-18 11:03:05 +0300

    add discardedField to loading editor ticks

--- src/Editor/Music.cpp
+++ src/Editor/Music.cpp
@@ -94,10 +94,13 @@ MusicImpl()
 
 	bool success;
 
-	success = myBeatTick.sound.load("assets/sound beat tick.wav", false, std::string(), std::string());
+	// used to ignore possible title/artist strings
+	std::string discardedField;
+
+	success = myBeatTick.sound.load("assets/sound beat tick.wav", false, discardedField, discardedField);
 	if(!success) HudError("%s", "Failed to load beat tick.\n");
 
-	success = myNoteTick.sound.load("assets/sound note tick.wav", false, std::string(), std::string());
+	success = myNoteTick.sound.load("assets/sound note tick.wav", false, discardedField, discardedField);
 	if(!success) HudError("%s", "Failed to load note tick.\n");
 }
 
