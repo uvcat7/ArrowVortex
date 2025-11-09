uvcat7/ArrowVortex

commit d87d5c2138b6e1449ac74b715e733045595c24c6

Freeform selection (#224)
* Freeform selection

* Fix tempo + region removal

* Update src/Dialogs/AdjustSync.cpp

src/Dialogs/AdjustSync.cpp
@@ -3,15 +3,18 @@
 #include <Core/StringUtils.h>
 #include <Core/WidgetsLayout.h>
 
-#include <Managers/TempoMan.h>
 #include <Managers/SimfileMan.h>
-#include <Simfile/SegmentGroup.h>
+#include <Managers/TempoMan.h>
 
-#include <Editor/View.h>
-#include <Editor/Selection.h>
+#include <Dialogs/Dialog.h>
+#include <Core/Widgets.h>
 #include <Editor/Common.h>
-#include <Editor/Editing.h>
+#include <Editor/FindTempo.h>
 #include <Editor/History.h>
+#include <Editor/Selection.h>
+#include <Simfile/Segments.h>
+#include <Simfile/Tempo.h>
+#include <string>
 
 namespace Vortex {
 
@@ -214,7 +217,7 @@ void DialogAdjustSync::onFindBPM() {
     if (!myTempoDetector) {
         myResetBPMDetection();
         auto region = gSelection->getSelectedRegion();
-        if (region.beginRow < region.endRow) {
+        if (!region.isOmni()) {
             myDetectionRow = region.beginRow;
             double time = gTempo->rowToTime(region.beginRow);
             double len = gTempo->rowToTime(region.endRow) - time;

src/Dialogs/GenerateNotes.cpp
@@ -1,18 +1,17 @@
 #include <Dialogs/GenerateNotes.h>
 
-#include <Core/Draw.h>
 #include <Core/WidgetsLayout.h>
 
+#include <Editor/Common.h>
 #include <Editor/Selection.h>
 #include <Editor/StreamGenerator.h>
-#include <Editor/Common.h>
-#include <Editor/Editor.h>
 
 #include <Core/Utils.h>
 
-#include <Managers/ChartMan.h>
-#include <Managers/StyleMan.h>
+#include <Core/Core.h>
+#include <Core/Widgets.h>
 #include <Managers/SimfileMan.h>
+#include <Managers/StyleMan.h>
 
 namespace Vortex {
 
@@ -93,7 +92,7 @@ void DialogGenerateNotes::myGenerateNotes() {
     auto region = gSelection->getSelectedRegion();
     if (gSimfile->isClosed()) {
         HudNote("%s", "Open a simfile or music file first.");
-    } else if (region.beginRow == region.endRow) {
+    } else if (region.isOmni()) {
         HudNote("%s", "Select a region first.");
     } else {
         streamGenerator_.generate(region.beginRow, region.endRow,

src/Editor/Editing.cpp
@@ -3,30 +3,42 @@
 #include <algorithm>
 #include <set>
 
-#include <Core/Utils.h>
 #include <Core/StringUtils.h>
+#include <Core/Utils.h>
 #include <Core/Xmr.h>
 
-#include <System/System.h>
 #include <System/Debug.h>
+#include <System/System.h>
 
 #include <Simfile/SegmentGroup.h>
 
-#include <Editor/Music.h>
-#include <Editor/Selection.h>
-#include <Editor/View.h>
-#include <Editor/Notefield.h>
-#include <Editor/History.h>
 #include <Editor/Common.h>
-#include <Editor/Editor.h>
+#include <Editor/History.h>
 #include <Editor/Menubar.h>
+#include <Editor/Music.h>
+#include <Editor/Notefield.h>
+#include <Editor/Selection.h>
+#include <Editor/TempoBoxes.h>
+#include <Editor/View.h>
 
-#include <Managers/MetadataMan.h>
+#include <Managers/SimfileMan.h>
 #include <Managers/StyleMan.h>
 #include <Managers/TempoMan.h>
-#include <Managers/SimfileMan.h>
 
-#include <Core/Draw.h>
+#include <cmath>
+#include <Core/Core.h>
+#include <Core/Input.h>
+#include <Core/Vector.h>
+#include <cstdint>
+#include <Managers/ChartMan.h>
+#include <Managers/NoteMan.h>
+#include <Simfile/Chart.h>
+#include <Simfile/Common.h>
+#include <Simfile/NoteList.h>
+#include <Simfile/Notes.h>
+#include <Simfile/Segments.h>
+#include <Simfile/Tempo.h>
+#include <string>
 
 namespace Vortex {
 
@@ -347,10 +359,26 @@ struct EditingImpl : public Editing {
     }
 
     void deleteSelection() override {
-        if (gSelection->getType() == Selection::TEMPO) {
+        if (gChart->isClosed()) {
+            return;
+        }
+
+        // There's four different cases that need addressing
+        if (gNotes->noneSelected() && gTempoBoxes->noneSelected()) {
+            // Nothing is selected, so delete region
+            gNotes->removeSelectedNotes();
+        } else if (!gNotes->noneSelected() && gTempoBoxes->noneSelected()) {
+            // Some notes are selected, delete only those, not the region
+            gNotes->removeSelectedNotes();
+        } else if (gNotes->noneSelected() && !gTempoBoxes->noneSelected()) {
+            // Only segments are chosen, delete those
             gTempo->removeSelectedSegments();
         } else {
+            // Both segments and notes are chosen, make this removal atomic
+            gHistory->startChain();
             gNotes->removeSelectedNotes();
+            gTempo->removeSelectedSegments();
+            gHistory->finishChain("Deleted selected objects");
         }
     }
 
@@ -383,9 +411,8 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, true, desc);
 
             // Reselect the notes.
-            if (gSelection->getType() == Selection::NOTES) {
-                gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size());
-            }
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
+                           false);
         } else {
             HudNote("There are no holds/rolls selected.");
         }
@@ -414,9 +441,8 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, false, &descs[type]);
 
             // Reselect the notes.
-            if (gSelection->getType() == Selection::NOTES) {
-                gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size());
-            }
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
+                           false);
         } else {
             HudNote("There are no holds/rolls selected.");
         }
@@ -444,9 +470,8 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, false, desc);
 
             // Reselect the notes.
-            if (gSelection->getType() == Selection::NOTES) {
-                gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size());
-            }
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
+                           false);
         } else {
             HudNote("There are no notes selected.");
         }
@@ -644,9 +669,7 @@ struct EditingImpl : public Editing {
         gNotes->modify(edit, false, descs + type);
 
         // Reselect the mirrored notes.
-        if (gSelection->getType() == Selection::NOTES) {
-            gNotes->select(SELECT_SET, edit.add.begin(), edit.rem.size());
-        }
+        gNotes->select(SELECT_SET, edit.add.begin(), edit.rem.size(), false);
     }
 
     void scaleNotes(int numerator, int denominator) override {
@@ -668,8 +691,9 @@ struct EditingImpl : public Editing {
 
         // If we are using region selection, we remove all expanded notes
         // outside the selection range.
-        if (gSelection->getType() == Selection::REGION) {
-            auto region = gSelection->getSelectedRegion();
+        auto region = gSelection->getSelectedRegion();
+
+        if (!region.isOmni()) {
             for (auto& it : edit.add) {
                 if (it.row > region.endRow) it.row = -1;
             }
@@ -686,9 +710,7 @@ struct EditingImpl : public Editing {
         gNotes->modify(edit, true, desc);
 
         // Reselect the scaled notes.
-        if (gSelection->getType() == Selection::NOTES) {
-            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size());
-        }
+        gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), false);
     }
 
     void insertRows(int row, int numRows, bool curChartOnly) override {
@@ -946,16 +968,23 @@ struct EditingImpl : public Editing {
     // EditingImpl :: clipboard functions.
 
     void copySelectionToClipboard(bool remove) {
-        if (gSelection->getType() == Selection::NONE) {
-            std::string time = Str::formatTime(gView->getCursorTime());
-            gSystem->setClipboardText(Str::fmt("%1").arg(time));
-            HudNote("Copied timestamp to Clipboard.");
-        } else if (gSelection->getType() == Selection::TEMPO) {
+        bool hasSelectedNotes = !gNotes->noneSelected();
+        bool hasSelectedSegments = !gTempoBoxes->noneSelected();
+
+        if (hasSelectedNotes && hasSelectedSegments) {
+            HudError(
+                "Both timing segments and notes selected, cannot copy both.");
+            return;
+        } else if (hasSelectedNotes && !hasSelectedSegments) {
+            gNotes->copyToClipboard(myUseTimeBasedCopy);
+            if (remove) gNotes->removeSelectedNotes();
+        } else if (!hasSelectedNotes && hasSelectedSegments) {
             gTempo->copyToClipboard();
             if (remove) gTempo->removeSelectedSegments();
         } else {
-            gNotes->copyToClipboard(myUseTimeBasedCopy);
-            if (remove) gNotes->removeSelectedNotes();
+            std::string time = Str::formatTime(gView->getCursorTime());
+            gSystem->setClipboardText(Str::fmt("%1").arg(time));
+            HudNote("Copied timestamp to Clipboard.");
         }
     }
 

src/Editor/Minimap.cpp
@@ -12,16 +12,16 @@
 
 #include <Editor/Common.h>
 #include <Editor/Editor.h>
-#include <Editor/View.h>
-#include <Editor/TextOverlay.h>
 #include <Editor/Menubar.h>
 #include <Editor/Selection.h>
+#include <Editor/TextOverlay.h>
+#include <Editor/View.h>
 
-#include <Managers/TempoMan.h>
-#include <Managers/SimfileMan.h>
 #include <Managers/ChartMan.h>
-#include <Managers/StyleMan.h>
 #include <Managers/NoteMan.h>
+#include <Managers/SimfileMan.h>
+#include <Managers/StyleMan.h>
+#include <Managers/TempoMan.h>
 
 namespace Vortex {
 
@@ -379,8 +379,8 @@ struct MinimapImpl : public Minimap {
         rect = myGetMapRect();
 
         // Draw the region selection if there is one.
-        if (gSelection->getType() == Selection::REGION) {
-            auto region = gSelection->getSelectedRegion();
+        auto region = gSelection->getSelectedRegion();
+        if (!region.isOmni()) {
             double top = gView->rowToOffset(region.beginRow);
             double btm = gView->rowToOffset(region.endRow);
             drawRegion(rect.x, rect.w, top, btm,

commit 42004849824f8352cf85bdb58ab86c068da5be81

Fix various selection bugs introduced in #224.

src/Dialogs/AdjustSync.cpp
@@ -217,7 +217,7 @@ void DialogAdjustSync::onFindBPM() {
     if (!myTempoDetector) {
         myResetBPMDetection();
         auto region = gSelection->getSelectedRegion();
-        if (!region.isOmni()) {
+        if (!region.isEmpty()) {
             myDetectionRow = region.beginRow;
             double time = gTempo->rowToTime(region.beginRow);
             double len = gTempo->rowToTime(region.endRow) - time;

src/Dialogs/GenerateNotes.cpp
@@ -92,7 +92,7 @@ void DialogGenerateNotes::myGenerateNotes() {
     auto region = gSelection->getSelectedRegion();
     if (gSimfile->isClosed()) {
         HudNote("%s", "Open a simfile or music file first.");
-    } else if (region.isOmni()) {
+    } else if (region.isEmpty()) {
         HudNote("%s", "Select a region first.");
     } else {
         streamGenerator_.generate(region.beginRow, region.endRow,

src/Editor/Editing.cpp
@@ -411,8 +411,7 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, false, desc);
 
             // Reselect the notes.
-            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
-                           false);
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), true);
         } else {
             HudNote("There are no holds/rolls selected.");
         }
@@ -441,8 +440,7 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, false, &descs[type]);
 
             // Reselect the notes.
-            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
-                           false);
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), true);
         } else {
             HudNote("There are no holds/rolls selected.");
         }
@@ -470,8 +468,7 @@ struct EditingImpl : public Editing {
             gNotes->modify(edit, false, desc);
 
             // Reselect the notes.
-            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(),
-                           false);
+            gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), true);
         } else {
             HudNote("There are no notes selected.");
         }
@@ -669,7 +666,7 @@ struct EditingImpl : public Editing {
         gNotes->modify(edit, false, descs + type);
 
         // Reselect the mirrored notes.
-        gNotes->select(SELECT_SET, edit.add.begin(), edit.rem.size(), false);
+        gNotes->select(SELECT_SET, edit.add.begin(), edit.rem.size(), true);
     }
 
     void scaleNotes(int numerator, int denominator) override {
@@ -692,10 +689,13 @@ struct EditingImpl : public Editing {
         // If we are using region selection, we remove all expanded notes
         // outside the selection range.
         auto region = gSelection->getSelectedRegion();
-
-        if (!region.isOmni()) {
+        bool truncated = false;
+        if (!region.isEmpty()) {
             for (auto& it : edit.add) {
-                if (it.row > region.endRow) it.row = -1;
+                if (it.row > region.endRow || it.row < region.beginRow) {
+                    truncated = true;
+                    it.row = -1;
+                }
             }
             edit.add.cleanup();
         }
@@ -703,14 +703,21 @@ struct EditingImpl : public Editing {
         // Perform the scale operation.
         static const NotesMan::EditDescription tExp = {"Expanded %1 note.",
                                                        "Expanded %1 notes."};
+        static const NotesMan::EditDescription tExpTrunc = {
+            "Expanded %1 note, bounded to the selection area.",
+            "Expanded %1 notes, bounded to the selection area."};
         static const NotesMan::EditDescription tCom = {"Compressed %1 note.",
                                                        "Compressed %1 notes."};
+        static const NotesMan::EditDescription tComTrunc = {
+            "Compressed %1 note, bounded to the selection area.",
+            "Compressed %1 notes, bounded to the selection area."};
         const NotesMan::EditDescription* desc =
-            (numerator > denominator) ? &tExp : &tCom;
+            (numerator > denominator) ? (truncated ? &tExpTrunc : &tExp)
+                                      : (truncated ? &tComTrunc : &tCom);
         gNotes->modify(edit, true, desc);
 
         // Reselect the scaled notes.
-        gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), false);
+        gNotes->select(SELECT_SET, edit.add.begin(), edit.add.size(), true);
     }
 
     void insertRows(int row, int numRows, bool curChartOnly) override {
@@ -968,13 +975,15 @@ struct EditingImpl : public Editing {
     // EditingImpl :: clipboard functions.
 
     void copySelectionToClipboard(bool remove) {
-        bool hasSelectedNotes = !gNotes->noneSelected();
+        bool hasSelectedNotes = !gNotes->noneSelected() ||
+                                !(gSelection->getSelectedRegion()).isEmpty();
         bool hasSelectedSegments = !gTempoBoxes->noneSelected();
 
         if (hasSelectedNotes && hasSelectedSegments) {
-            HudError(
-                "Both timing segments and notes selected, cannot copy both.");
-            return;
+            HudWarning(
+                "Both timing segments and notes selected, copying notes only.");
+            gNotes->copyToClipboard(myUseTimeBasedCopy);
+            if (remove) gNotes->removeSelectedNotes();
         } else if (hasSelectedNotes && !hasSelectedSegments) {
             gNotes->copyToClipboard(myUseTimeBasedCopy);
             if (remove) gNotes->removeSelectedNotes();

src/Editor/Minimap.cpp
@@ -380,7 +380,7 @@ struct MinimapImpl : public Minimap {
 
         // Draw the region selection if there is one.
         auto region = gSelection->getSelectedRegion();
-        if (!region.isOmni()) {
+        if (!region.isEmpty()) {
             double top = gView->rowToOffset(region.beginRow);
             double btm = gView->rowToOffset(region.endRow);
             drawRegion(rect.x, rect.w, top, btm,

src/Editor/Selection.cpp
@@ -1,3 +1,4 @@
+#include <Editor/Editor.h>
 #include <Editor/Selection.h>
 
 #include <Core/Draw.h>
@@ -76,7 +77,6 @@ struct SelectionImpl : public Selection {
             gNotes->deselectAll();
             gTempoBoxes->deselectAll();
             this->setRegion(0, 0);
-
             evt.setHandled();
         }
     }
@@ -102,6 +102,13 @@ struct SelectionImpl : public Selection {
 
             selectTempoBoxes(mod, t, b, l, r);
             selectNotes(mod, t, b, l, r);
+
+            // Clear the selection region if we didn't select anything.
+            if (mod == SELECT_SET && gNotes->noneSelected() &&
+                gTempoBoxes->noneSelected()) {
+                this->setRegion(0, 0);
+                gEditor->reportChanges(VCM_SELECTION_CHANGED);
+            }
 
             myIsDraggingSelection = false;
         }
@@ -193,18 +200,19 @@ struct SelectionImpl : public Selection {
             }
             selectNotes(mod, begin, end, true);
         }
+        gEditor->reportChanges(VCM_SELECTION_CHANGED);
     }
 
     void drawRegionSelection() override {
         // Draw area selection box.
-        if (myIsSelectingRegion || !myRegion.isOmni()) {
+        if (myIsSelectingRegion || !myRegion.isEmpty()) {
             uint32_t outline = RGBAtoColor32(153, 255, 153, 153);
             auto coords = gView->getReceptorCoords();
             int x = coords.xl, w = coords.xr - coords.xl;
             if (myIsSelectingRegion) {
                 int y = gView->rowToY(myRegion.beginRow);
                 Draw::fill({x, y - 1, w, 2}, outline);
-            } else if (!myRegion.isOmni()) {
+            } else if (!myRegion.isEmpty()) {
                 int t = gView->rowToY(myRegion.beginRow);
                 int b = gView->rowToY(myRegion.endRow);
                 Draw::fill({x, t, w, b - t}, RGBAtoColor32(153, 255, 153, 90));
@@ -298,16 +306,17 @@ struct SelectionImpl : public Selection {
     }
 
     int getSelectedNotes(NoteList& out) override {
-        if (myRegion.isOmni()) {
-            for (auto& note : *gNotes) {
-                if (note.isSelected) out.append(CompressNote(note));
-            }
-        } else {
+        // Get both manually selected notes and the region, since they're
+        // independent now
+        if (!myRegion.isEmpty()) {
             auto note = gNotes->begin(), end = gNotes->end();
             for (; note != end && note->row < myRegion.beginRow; ++note);
             for (; note != end && note->row <= myRegion.endRow; ++note) {
-                out.append(CompressNote(*note));
+                if (!note->isSelected) out.append(CompressNote(*note));
             }
+        }
+        for (auto& note : *gNotes) {
+            if (note.isSelected) out.append(CompressNote(note));
         }
         return out.size();
     }
@@ -329,9 +338,11 @@ struct SelectionImpl : public Selection {
     void selectRegion(int row, int endRow) override {
         setRegion(row, endRow);
 
+        auto firstRow = std::min(row, endRow);
+        auto lastRow = std::max(row, endRow);
         if (row != endRow) {
-            double m1 = gTempo->beatToMeasure(row * BEATS_PER_ROW);
-            double m2 = gTempo->beatToMeasure(endRow * BEATS_PER_ROW);
+            double m1 = gTempo->beatToMeasure(firstRow * BEATS_PER_ROW);
+            double m2 = gTempo->beatToMeasure(lastRow * BEATS_PER_ROW);
 
             Str::fmt fmt("Selected measure %1 to %2 (%3 measures)");
             fmt.arg(m1, 0, 2).arg(m2, 0, 2).arg(m2 - m1, 0, 2);

src/Editor/Selection.h
@@ -13,10 +13,10 @@ struct SelectionRegion {
     int beginRow;
     int endRow;
 
-    const bool isOmni() const { return this->beginRow == this->endRow; }
+    const bool isEmpty() const { return this->beginRow == this->endRow; }
 
     const bool rowIsInRegion(int row) const {
-        return this->isOmni() || this->beginRow <= row && row <= this->endRow;
+        return this->isEmpty() || this->beginRow <= row && row <= this->endRow;
     }
 };
 

src/Editor/View.cpp
@@ -449,7 +449,7 @@ struct ViewImpl : public View, public InputHandler {
         if (gView->hasReverseScroll()) top = !top;
 
         auto region = gSelection->getSelectedRegion();
-        if (!region.isOmni()) {
+        if (!region.isEmpty()) {
             setCursorRow(top ? region.beginRow : region.endRow);
         } else {
             NoteList notes;

src/Managers/NoteMan.cpp
@@ -242,7 +242,7 @@ struct NotesManImpl : public NotesMan {
         }
 
         if (myChart == chart) {
-            if (!firstTime) select(SELECT_SET, add.begin(), add.size(), false);
+            if (!firstTime) select(SELECT_SET, add.begin(), add.size(), true);
 
             if (!updated) myUpdateNotes();
 
@@ -665,9 +665,9 @@ struct NotesManImpl : public NotesMan {
 
         for (auto& note : myNotes) {
             bool isEligible =
-                (hasSelectedNotes   ? note.isSelected
-                 : !region.isOmni() ? region.rowIsInRegion(note.row)
-                                    : false);
+                (hasSelectedNotes    ? note.isSelected
+                 : !region.isEmpty() ? region.rowIsInRegion(note.row)
+                                     : false);
             if (isEligible) {
                 edit.rem.append(CompressNote(note));
             }
@@ -732,7 +732,7 @@ struct NotesManImpl : public NotesMan {
         static const NotesMan::EditDescription desc = {"Pasted %1 note",
                                                        "Pasted %1 notes"};
         modify(edit, !insert, &desc);
-        this->select(SELECT_SET, edit.add.begin(), edit.add.size(), false);
+        this->select(SELECT_SET, edit.add.begin(), edit.add.size(), true);
     }
 
     // ================================================================================================