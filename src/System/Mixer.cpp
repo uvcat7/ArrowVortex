7082952f04ddc2f0ab94b81481945042996619d3
uvcat <theultravioletcatastrophe10@gmail.com>
2026-04-22 23:02:56 -0700

    ported Windows mixer to SDL_Audio

--- src/System/Mixer.cpp
+++ src/System/Mixer.cpp
@@ -1,97 +1,100 @@
 #include <System/Mixer.h>
-#include <SDL3_mixer/SDL_mixer.h>
-#include <filesystem>
-#include <vector>
-namespace fs = std::filesystem;
+#include <SDL3/SDL_audio.h>
+#include <SDL3/SDL_stdinc.h>
+#include <Editor/Common.h>
+
+#include <malloc.h>
 
 namespace Vortex {
 
+static const int WAVEOUT_CHANNELS = 2;
+static const int WAVEOUT_BLOCKS = 8;
+static const int WAVEOUT_BLOCK_FRAMES = 8192;
+static const int WAVEOUT_BLOCK_SIZE =
+    sizeof(short) * WAVEOUT_CHANNELS * WAVEOUT_BLOCK_FRAMES;
+
 // ================================================================================================
 // MixerImpl :: member data.
+SDL_AudioStream* myAudioStream = nullptr;
+MixSource* mySource;
+void SDLCALL audio_callback(void* userdata, SDL_AudioStream* stream,
+                            int additional_amount, int total_amount) {
+    int remaining = additional_amount;
+    short samples[WAVEOUT_BLOCK_SIZE];
+
+    while (remaining > 0) {
+        int current = std::min(WAVEOUT_BLOCK_SIZE, remaining);
+
+        mySource->writeFrames(samples,
+                              current / WAVEOUT_CHANNELS / sizeof(short));
+        SDL_PutAudioStreamData(myAudioStream, samples, current);
+        remaining -= current;
+    }
+}
 
 struct MixerImpl : public Mixer {
-    MIX_Mixer* mixer = nullptr;
-    MIX_Track* music_track = nullptr;
-    MIX_Audio* music = nullptr;
-    std::vector<MIX_Audio*> sfx;
-    std::vector<MIX_Track*> sfx_tracks;
-
     int myFreeBlockIndex = 0;
+
+    SDL_AudioSpec myAudioSpec = {SDL_AUDIO_S16LE, WAVEOUT_CHANNELS, 0};
+    SDL_AudioDeviceID myDeviceId = 0;
 
     char* myBlockMemory = nullptr;
 
-    MixSource* mySource;
+    bool myPauseThread = false;
+    bool myIsOpened = false;
 
     // ================================================================================================
     // MixerImpl :: constructor and destructor.
 
-    ~MixerImpl() override {
-        close();
-        for (MIX_Audio* s : sfx) {
-            MIX_DestroyAudio(s);
-        }
-        for (MIX_Track* t : sfx_tracks) {
-            MIX_DestroyTrack(t);
-        }
-        sfx.clear();
-    }
+    ~MixerImpl() override { close(); }
 
     MixerImpl() {
-        if (MIX_Init())
-            mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK,
-                                          nullptr);
-        if (!mixer) {
-            HudError("Failed to create mixer with error %s.\n", SDL_GetError());
-        };
-        sfx.clear();
-        open_sfx(fs::path("assets/sound beat tick.wav"));
-        open_sfx(fs::path("assets/sound note tick.wav"));
+        mySource = nullptr;
+        myBlockMemory = static_cast<char*>(
+            SDL_aligned_alloc(WAVEOUT_BLOCK_SIZE * WAVEOUT_BLOCKS, 16));
     }
-
-    bool is_initialized() override { return mixer != nullptr; }
 
     void close() override {
-        MIX_StopAllTracks(mixer, 0);
-        MIX_DestroyTrack(music_track);
-        MIX_DestroyAudio(music);
+        SDL_PauseAudioStreamDevice(myAudioStream);
+        SDL_DestroyAudioStream(myAudioStream);
+        if (myDeviceId) {
+            SDL_CloseAudioDevice(myDeviceId);
+            myDeviceId = 0;
+        }
+        myFreeBlockIndex = 0;
+        myIsOpened = false;
+        myPauseThread = true;
     }
 
-    bool open_sfx(fs::path path) override {
-        MIX_Audio* s = MIX_LoadAudio(mixer, path.string().c_str(), true);
-        MIX_Track* t = MIX_CreateTrack(mixer);
-        MIX_SetTrackAudio(t, s);
-        sfx.push_back(s);
-        sfx_tracks.push_back(t);
-        return s != nullptr;
-    }
+    bool open(MixSource* source, int samplerate) override {
+        if (myIsOpened) close();
 
-    void play_sfx(SoundEffects s, float volume) override {
-        MIX_Track* track = sfx_tracks.at(static_cast<int>(s));
-        if (!track) return;
-        MIX_SetTrackGain(track, volume);
-        if (!MIX_PlayTrack(music_track, 0))
-            HudError("Failed to start sfx #d playback with error %s",
-                     static_cast<int>(s), SDL_GetError());
-    }
-
-    bool open(fs::path path) override {
-        music = MIX_LoadAudio(mixer, path.string().c_str(), true);
-        music_track = MIX_CreateTrack(mixer);
-        MIX_SetTrackAudio(music_track, music);
-        return music != nullptr;
-    }
-
-    void pause() override { MIX_PauseAllTracks(mixer); }
-
-    void resume(int64_t ms, float rate, float volume) override {
-        if (!MIX_PlayTrack(music_track, 0))
-            HudError("Failed to start music playback with error %s",
+        myAudioSpec.freq = samplerate;
+        myAudioStream = SDL_OpenAudioDeviceStream(
+            SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &myAudioSpec, audio_callback,
+            &myBlockMemory);
+        if (!myAudioStream) {
+            HudError("Failed to open audio device stream with error %s.\n",
                      SDL_GetError());
-        int64_t frames = MIX_TrackMSToFrames(music_track, ms);
-        MIX_SetTrackFrequencyRatio(music_track, rate);
-        MIX_SetTrackGain(music_track, volume);
-        MIX_SetTrackPlaybackPosition(music_track, frames);
-        HudWarning("Mixer::resume: %d, %d", ms, frames);
+            return false;
+        }
+        mySource = source;
+        myIsOpened = true;
+        return true;
+    }
+
+    void pause() override {
+        if (myIsOpened && !myPauseThread) {
+            myPauseThread = true;
+            SDL_PauseAudioStreamDevice(myAudioStream);
+        }
+    }
+
+    void resume() override {
+        if (myIsOpened && myPauseThread) {
+            myPauseThread = false;
+            SDL_ResumeAudioStreamDevice(myAudioStream);
+        }
     }
 
 };  // MixerImpl
--- src/System/Mixer.h
+++ src/System/Mixer.h
@@ -1,29 +1,16 @@
-#include <Core/Core.h>
-#include <filesystem>
-#include <SDL3_mixer/SDL_mixer.h>
-namespace fs = std::filesystem;
-
-namespace Vortex {
-
-enum SoundEffects { kNoteTick, kBeatTick };
 struct MixSource {
     virtual void writeFrames(short* buffer, int frames) = 0;
 };
 
+namespace Vortex {
 struct Mixer {
     static Mixer* create();
 
     virtual ~Mixer();
 
-    virtual bool is_initialized() = 0;
-
-    virtual bool open_sfx(fs::path path) = 0;
-
-    virtual void play_sfx(SoundEffects s, float volume) = 0;
-
     /// Opens the mixer for audio output at the given samplerate. The mixer is
     /// initially paused.
-    virtual bool open(fs::path path) = 0;
+    virtual bool open(MixSource* source, int samplerate) = 0;
 
     /// Stops audio output and closes the mixer, until open is called again.
     virtual void close() = 0;
@@ -32,7 +19,6 @@ struct Mixer {
     virtual void pause() = 0;
 
     /// Unpauses the audio mixer and starts playing samples from the mix source.
-    virtual void resume(int64_t ms, float rate, float volume) = 0;
+    virtual void resume() = 0;
 };
-
-};  // namespace Vortex
+}  // namespace Vortex
