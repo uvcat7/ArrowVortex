2c965931b4e5d6372d513e67410103d90344e0e6
Psycast <jonathanjwhite@hotmail.com>
2026-08-25 06:35:23 -0230

    Restore Right, Middle, and double click handling.

--- src/System/System.cpp
+++ src/System/System.cpp
@@ -820,32 +820,37 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
             }
             break;
         }
-        case SDL_EVENT_MOUSE_BUTTON_DOWN:
-            if (event->button.button == SDL_BUTTON_LEFT) {
+        case SDL_EVENT_MOUSE_BUTTON_DOWN: {
+            mc = event->button.button;
+            if (mc >= SDL_BUTTON_LEFT && mc <= SDL_BUTTON_RIGHT) {
                 SDL_CaptureMouse(true);
                 if (myIsInsideMessageLoop) {
+                    auto mdc = mcodes[mc];
                     float x, y;
                     SDL_GetMouseState(&x, &y);
-                    myEvents.addMousePress(Mouse::LMB, static_cast<int>(x),
-                                           static_cast<int>(y),
-                                           gSystem->getKeyFlags(), false);
-                    myMouseState.set(Mouse::LMB);
+                    myEvents.addMousePress(
+                        mcodes[mc], static_cast<int>(x), static_cast<int>(y),
+                        gSystem->getKeyFlags(), event->button.clicks >= 2);
+                    myMouseState.set(mcodes[mc]);
                 }
             }
             break;
-        case SDL_EVENT_MOUSE_BUTTON_UP:
-            if (event->button.button == SDL_BUTTON_LEFT) {
+        }
+        case SDL_EVENT_MOUSE_BUTTON_UP: {
+            mc = event->button.button;
+            if (mc >= SDL_BUTTON_LEFT && mc <= SDL_BUTTON_RIGHT) {
                 SDL_CaptureMouse(false);
                 if (myIsInsideMessageLoop) {
                     float x, y;
                     SDL_GetMouseState(&x, &y);
-                    myEvents.addMouseRelease(Mouse::LMB, static_cast<int>(x),
+                    myEvents.addMouseRelease(mcodes[mc], static_cast<int>(x),
                                              static_cast<int>(y),
                                              gSystem->getKeyFlags());
-                    myMouseState.reset(Mouse::LMB);
+                    myMouseState.reset(mcodes[mc]);
                 }
             }
             break;
+        }
         case SDL_EVENT_TEXT_INPUT: {
             const char* wp = event->text.text;
             const char n = '\n';
