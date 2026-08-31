54844bcd7574336b90d5ab2012f1824917e2edc2
Psycast <jonathanjwhite@hotmail.com>
2026-08-31 04:04:16 -0230

    Add rebindable keys for Visual Sync
    We ignore keyflags on release since modifier keys can't be bound alone, and may be released after initial press, so we only care for the primary activator key.
    
    Need to check for both sync mode separately as a rebound key may be 2 separate primary keys.

--- src/Editor/Notefield.h
+++ src/Editor/Notefield.h
@@ -25,19 +25,20 @@ struct Notefield {
     virtual void toggleShowBeatLinesSnap() = 0;
     virtual void toggleShowBeatLinesColor() = 0;
     virtual void toggleShowBeatLinesHover() = 0;
-    virtual void clearVisualSyncBeatlinePreset() = 0;
-    virtual void setVisualSyncBeatlinePreset() = 0;
     virtual void toggleShowNotes() = 0;
     virtual void toggleShowSongPreview() = 0;
+    virtual void setVisualSyncBeatline(bool value) = 0;
 
     virtual bool hasShowWaveform() = 0;
     virtual bool hasShowBeatLines() = 0;
     virtual bool hasShowBeatLinesSnap() = 0;
     virtual bool hasShowBeatLinesColor() = 0;
     virtual bool hasShowBeatLinesHover() = 0;
-    virtual bool hasVisualSyncBeatlinePreset() = 0;
+    virtual bool hasVisualSyncBeatline() = 0;
     virtual bool hasShowNotes() = 0;
     virtual bool hasShowSongPreview() = 0;
+
+    virtual bool needVisualSyncBeatlines() = 0;
 };
 
 extern Notefield* gNotefield;
