f2ec7a4d1f2db5d189a192fa5202ddd44f30ee05
Psycast <jonathanjwhite@hotmail.com>
2026-09-12 16:26:18 -0230

    Always reset region selection.
    It should clear both set and mid selection of regions.
    ¯\_(ツ)_/¯

--- src/Editor/Selection.cpp
+++ src/Editor/Selection.cpp
@@ -136,10 +136,8 @@ struct SelectionImpl : public Selection {
             if (mod == SELECT_SET) {
                 // Clear the selection region if we didn't select anything.
                 if (notes == 0 && tempos == 0) {
-                    if (myIsSelectingRegion) {
-                        setRegion(0, 0);
-                        myIsSelectingRegion = false;
-                    }
+                    setRegion(0, 0);
+                    myIsSelectingRegion = false;
                     gEditor->reportChanges(VCM_SELECTION_CHANGED);
                 }
 
